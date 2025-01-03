82e5f7fbaee081767ec22cd3cf28ddb16e38286a
Mikhail <m@datatera.io>
2025-01-03 23:55:20 +0100

    use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,42 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockExtractions = [
-  { id: 1, name: "Extraction 1", description: "Description 1", defaults: { enrich: false, merge: false }, template: "" },
-];
+// const mockExtractions = [
+//   { id: 1, name: "Extraction 1", description: "Description 1", defaults: { enrich: false, merge: false }, template: "" },
+// ];
 
-
-export default async function HomePage() {
+async function Extractions() {
   const extractions = await db.query.extractions.findMany({
     orderBy: (extractions, { desc }) => [desc(extractions.createdAt)],
   });
   console.log(extractions);
   
   return (
+    <div className="flex flex-wrap gap-4">
+      {extractions.map((extraction, index: number) => (
+        <div key={extraction.id + "-" + index} className="flex w-48 flex-col">
+          <h2>{extraction.name}</h2>
+          <p>{extraction.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {extractions.map((extraction, index) => (
-          <div key={extraction.id + "-" + index} className="flex w-48 flex-col">
-            <h2>{extraction.name}</h2>
-            <p>{extraction.description}</p>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">Please sign in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Extractions />
+      </SignedIn>
     </main>
   );
 }
