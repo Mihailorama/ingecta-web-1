659a93958165032669a2b7d84506b5c2ef20e0f3
Mikhail <m@datatera.io>
2025-01-03 01:03:06 +0100

    adding clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,16 @@
 import "~/styles/globals.css";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 import { GeistSans } from "geist/font/sans";
+import { Inter } from "next/font/google";
 import { type Metadata } from "next";
+import { TopNav } from "./_components/topnav";
+
+const inter = Inter({ 
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Web Data Extractor",
@@ -9,24 +18,17 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-export function TopNav() {
-  return (
-    <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold">
-      <div>Web Data Extractor</div>
-      <div>Sign In</div>
-    </nav>
-  );
-}
-
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${GeistSans.variable}`}>
-        <TopNav />
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`font-sans ${GeistSans.variable}`}>
+          <TopNav />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
